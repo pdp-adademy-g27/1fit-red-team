63cdfdeda8ba92100b8ed7658ff1f3e3ede082c9
aziza <yuldasheva2210@gmail.com>
2024-01-24 17:38:23 +0500

    initial commit

---
+++ src/main/java/com/example/onefitclone/OneFitCloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.onefitclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneFitCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneFitCloneApplication.class, args);
+    }
+
+}
