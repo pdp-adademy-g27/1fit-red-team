00d09b08bbb4de317c96e92b4b328bc3fec0616d
Behzod <alzosidi@gmail.com>
2024-01-25 12:13:40 +0500

    test_one

--- src/main/java/com/example/onefitclone/OneFitCloneApplication.java
+++ src/main/java/com/example/onefitclone/OneFitCloneApplication.java
@@ -8,6 +8,7 @@ public class OneFitCloneApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OneFitCloneApplication.class, args);
+        System.out.println("test");
     }
 
 }
