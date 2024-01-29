3e8ef0af26f2656c298ee47a502949dd867130b6
Husniddinbek <hsopoyev11@gmail.com>
2024-01-30 02:11:50 +0500

    Create Entities

---
+++ src/main/java/com/example/onefitclone/feedback/entity/Feedback.java
@@ -0,0 +1,18 @@
+package com.example.onefitclone.feedback.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Feedback {
+    @Id
+    private UUID id;
+}
