46cc23f640b8f399c2c5c17721996f29735c92e2
Kimyokhon <nkimyoxon@gmail.com>
2024-01-30 08:54:45 +0500

    USER CRUD togirlandi

--- src/main/java/com/example/onefitclone/user/UserDtoMapper.java
+++ src/main/java/com/example/onefitclone/user/UserDtoMapper.java
@@ -6,25 +6,29 @@ import com.example.onefitclone.user.dto.UserResponseDto;
 import com.example.onefitclone.user.dto.UserUpdateDto;
 import com.example.onefitclone.user.entity.User;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 
 @Component
 @RequiredArgsConstructor
 public class UserDtoMapper extends GenericMapper<User, UserCreatedDto, UserResponseDto, UserUpdateDto> {
-
+    private final ModelMapper mapper;
     @Override
     public User toEntity(UserCreatedDto userCreatedDto) {
-        return null;
+
+        return mapper.map(userCreatedDto,User.class);
     }
 
     @Override
     public UserResponseDto toResponseDto(User user) {
-        return null;
+
+        return mapper.map(user,UserResponseDto.class);
     }
 
     @Override
     public void toEntity(UserUpdateDto userUpdateDto, User user) {
+        mapper.map(userUpdateDto,user);
 
     }
 }
