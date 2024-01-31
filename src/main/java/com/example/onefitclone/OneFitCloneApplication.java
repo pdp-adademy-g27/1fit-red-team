be5f645e1badc5178d78c115af371ca8cd4bec6a
Kimyokhon <nkimyoxon@gmail.com>
2024-01-31 19:52:43 +0500

    USER CRUD togirlandi

--- src/main/java/com/example/onefitclone/OneFitCloneApplication.java
+++ src/main/java/com/example/onefitclone/OneFitCloneApplication.java
@@ -2,8 +2,6 @@ package com.example.onefitclone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 public class OneFitCloneApplication {
